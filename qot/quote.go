18c8619907f7366131e9ab8e747dbdf77e10eb9f
Terence Chan <shing1211@gmail.com>
2026-04-07 11:24:09 +0800

    feat: add Qot depth APIs
    - GetOrderBook (2106) - order book
    - GetTicker (2107) - tick-by-tick trades
    - GetRT (2108) - real-time minute data
    - GetBroker (2111) - broker queue

--- qot/quote.go
+++ qot/quote.go
@@ -9,13 +9,22 @@ import (
 	"github.com/futuopen/ftapi4go/pb/common"
 	"github.com/futuopen/ftapi4go/pb/qotcommon"
 	"github.com/futuopen/ftapi4go/pb/qotgetbasicqot"
+	"github.com/futuopen/ftapi4go/pb/qotgetbroker"
 	"github.com/futuopen/ftapi4go/pb/qotgetkl"
+	"github.com/futuopen/ftapi4go/pb/qotgetorderbook"
+	"github.com/futuopen/ftapi4go/pb/qotgetrt"
+	"github.com/futuopen/ftapi4go/pb/qotgetticker"
 )
 
-// Protocol IDs for Qot (Market Data)
 const (
-	ProtoID_GetBasicQot = 2101
-	ProtoID_GetKL       = 2102
+	ProtoID_GetBasicQot       = 2101
+	ProtoID_GetKL             = 2102
+	ProtoID_GetHistoryKL      = 2103
+	ProtoID_GetOrderBook      = 2106
+	ProtoID_GetTicker         = 2107
+	ProtoID_GetRT             = 2108
+	ProtoID_GetMarketSnapshot = 2109
+	ProtoID_GetBroker         = 2111
 )
 
 type BasicQot struct {
@@ -29,28 +38,12 @@ type BasicQot struct {
 	Turnover  float64
 }
 
-type KLine struct {
-	Time           string
-	IsBlank        bool
-	HighPrice      float64
-	OpenPrice      float64
-	LowPrice       float64
-	ClosePrice     float64
-	LastClosePrice float64
-	Volume         int64
-	Turnover       float64
-	ChangeRate     float64
-	Timestamp      float64
-}
-
 func GetBasicQot(c *futuapi.Client, securityList []*qotcommon.Security) ([]*BasicQot, error) {
 	c2s := &qotgetbasicqot.C2S{
 		SecurityList: securityList,
 	}
 
-	pkt := &qotgetbasicqot.Request{
-		C2S: c2s,
-	}
+	pkt := &qotgetbasicqot.Request{C2S: c2s}
 
 	body, err := proto.Marshal(pkt)
 	if err != nil {
@@ -98,10 +91,24 @@ func GetBasicQot(c *futuapi.Client, securityList []*qotcommon.Security) ([]*Basi
 	return result, nil
 }
 
+type KLine struct {
+	Time           string
+	IsBlank        bool
+	HighPrice      float64
+	OpenPrice      float64
+	LowPrice       float64
+	ClosePrice     float64
+	LastClosePrice float64
+	Volume         int64
+	Turnover       float64
+	ChangeRate     float64
+	Timestamp      float64
+}
+
 type GetKLRequest struct {
 	Security  *qotcommon.Security
-	RehabType int32 // qotcommon.RehabType
-	KLType    int32 // qotcommon.KLType
+	RehabType int32
+	KLType    int32
 	ReqNum    int32
 }
 
@@ -169,6 +176,342 @@ func GetKL(c *futuapi.Client, req *GetKLRequest) (*GetKLResponse, error) {
 			Turnover:       kl.GetTurnover(),
 			ChangeRate:     kl.GetChangeRate(),
 			Timestamp:      kl.GetTimestamp(),
+		})
+	}
+
+	return result, nil
+}
+
+type OrderBook struct {
+	Price      float64
+	Volume     int64
+	OrderCount int32
+	DetailList []*OrderBookDetail
+}
+
+type OrderBookDetail struct {
+	Price   float64
+	Volume  int64
+	OrderID int64
+}
+
+type GetOrderBookRequest struct {
+	Security *qotcommon.Security
+	Num      int32
+}
+
+type GetOrderBookResponse struct {
+	Security                *qotcommon.Security
+	Name                    string
+	OrderBookAskList        []*OrderBook
+	OrderBookBidList        []*OrderBook
+	SvrRecvTimeBid          string
+	SvrRecvTimeBidTimestamp float64
+	SvrRecvTimeAsk          string
+	SvrRecvTimeAskTimestamp float64
+}
+
+func GetOrderBook(c *futuapi.Client, req *GetOrderBookRequest) (*GetOrderBookResponse, error) {
+	c2s := &qotgetorderbook.C2S{
+		Security: req.Security,
+		Num:      &req.Num,
+	}
+
+	pkt := &qotgetorderbook.Request{C2S: c2s}
+
+	body, err := proto.Marshal(pkt)
+	if err != nil {
+		return nil, err
+	}
+
+	serialNo := c.NextSerialNo()
+	if err := c.Conn().WritePacket(ProtoID_GetOrderBook, serialNo, body); err != nil {
+		return nil, err
+	}
+
+	pktResp, err := c.Conn().ReadPacket()
+	if err != nil {
+		return nil, err
+	}
+
+	var rsp qotgetorderbook.Response
+	if err := proto.Unmarshal(pktResp.Body, &rsp); err != nil {
+		return nil, err
+	}
+
+	if rsp.GetRetType() != int32(common.RetType_RetType_Succeed) {
+		return nil, fmt.Errorf("GetOrderBook failed: retType=%d, retMsg=%s", rsp.GetRetType(), rsp.GetRetMsg())
+	}
+
+	s2c := rsp.GetS2C()
+	if s2c == nil {
+		return nil, fmt.Errorf("GetOrderBook: s2c is nil")
+	}
+
+	result := &GetOrderBookResponse{
+		Security:                s2c.GetSecurity(),
+		Name:                    s2c.GetName(),
+		SvrRecvTimeBid:          s2c.GetSvrRecvTimeBid(),
+		SvrRecvTimeBidTimestamp: s2c.GetSvrRecvTimeBidTimestamp(),
+		SvrRecvTimeAsk:          s2c.GetSvrRecvTimeAsk(),
+		SvrRecvTimeAskTimestamp: s2c.GetSvrRecvTimeAskTimestamp(),
+	}
+
+	for _, ob := range s2c.GetOrderBookAskList() {
+		result.OrderBookAskList = append(result.OrderBookAskList, &OrderBook{
+			Price:      ob.GetPrice(),
+			Volume:     ob.GetVolume(),
+			OrderCount: ob.GetOrederCount(),
+		})
+	}
+
+	for _, ob := range s2c.GetOrderBookBidList() {
+		result.OrderBookBidList = append(result.OrderBookBidList, &OrderBook{
+			Price:      ob.GetPrice(),
+			Volume:     ob.GetVolume(),
+			OrderCount: ob.GetOrederCount(),
+		})
+	}
+
+	return result, nil
+}
+
+type Ticker struct {
+	Time      string
+	Sequence  int64
+	Dir       int32
+	Price     float64
+	Volume    int64
+	Turnover  float64
+	RecvTime  float64
+	Type      int32
+	TypeSign  int32
+	Timestamp float64
+}
+
+type GetTickerRequest struct {
+	Security *qotcommon.Security
+	Num      int32
+}
+
+type GetTickerResponse struct {
+	Security   *qotcommon.Security
+	Name       string
+	TickerList []*Ticker
+}
+
+func GetTicker(c *futuapi.Client, req *GetTickerRequest) (*GetTickerResponse, error) {
+	maxRetNum := req.Num
+	c2s := &qotgetticker.C2S{
+		Security:  req.Security,
+		MaxRetNum: &maxRetNum,
+	}
+
+	pkt := &qotgetticker.Request{C2S: c2s}
+
+	body, err := proto.Marshal(pkt)
+	if err != nil {
+		return nil, err
+	}
+
+	serialNo := c.NextSerialNo()
+	if err := c.Conn().WritePacket(ProtoID_GetTicker, serialNo, body); err != nil {
+		return nil, err
+	}
+
+	pktResp, err := c.Conn().ReadPacket()
+	if err != nil {
+		return nil, err
+	}
+
+	var rsp qotgetticker.Response
+	if err := proto.Unmarshal(pktResp.Body, &rsp); err != nil {
+		return nil, err
+	}
+
+	if rsp.GetRetType() != int32(common.RetType_RetType_Succeed) {
+		return nil, fmt.Errorf("GetTicker failed: retType=%d, retMsg=%s", rsp.GetRetType(), rsp.GetRetMsg())
+	}
+
+	s2c := rsp.GetS2C()
+	if s2c == nil {
+		return nil, fmt.Errorf("GetTicker: s2c is nil")
+	}
+
+	result := &GetTickerResponse{
+		Security:   s2c.GetSecurity(),
+		Name:       s2c.GetName(),
+		TickerList: make([]*Ticker, 0, len(s2c.GetTickerList())),
+	}
+
+	for _, t := range s2c.GetTickerList() {
+		result.TickerList = append(result.TickerList, &Ticker{
+			Time:      t.GetTime(),
+			Sequence:  t.GetSequence(),
+			Dir:       t.GetDir(),
+			Price:     t.GetPrice(),
+			Volume:    t.GetVolume(),
+			Turnover:  t.GetTurnover(),
+			RecvTime:  t.GetRecvTime(),
+			Type:      t.GetType(),
+			TypeSign:  t.GetTypeSign(),
+			Timestamp: t.GetTimestamp(),
+		})
+	}
+
+	return result, nil
+}
+
+type RT struct {
+	Time     string
+	Price    float64
+	Volume   int64
+	Turnover float64
+	AvgPrice float64
+}
+
+type GetRTRequest struct {
+	Security *qotcommon.Security
+}
+
+type GetRTResponse struct {
+	Security *qotcommon.Security
+	Name     string
+	RTList   []*RT
+}
+
+func GetRT(c *futuapi.Client, req *GetRTRequest) (*GetRTResponse, error) {
+	c2s := &qotgetrt.C2S{
+		Security: req.Security,
+	}
+
+	pkt := &qotgetrt.Request{C2S: c2s}
+
+	body, err := proto.Marshal(pkt)
+	if err != nil {
+		return nil, err
+	}
+
+	serialNo := c.NextSerialNo()
+	if err := c.Conn().WritePacket(ProtoID_GetRT, serialNo, body); err != nil {
+		return nil, err
+	}
+
+	pktResp, err := c.Conn().ReadPacket()
+	if err != nil {
+		return nil, err
+	}
+
+	var rsp qotgetrt.Response
+	if err := proto.Unmarshal(pktResp.Body, &rsp); err != nil {
+		return nil, err
+	}
+
+	if rsp.GetRetType() != int32(common.RetType_RetType_Succeed) {
+		return nil, fmt.Errorf("GetRT failed: retType=%d, retMsg=%s", rsp.GetRetType(), rsp.GetRetMsg())
+	}
+
+	s2c := rsp.GetS2C()
+	if s2c == nil {
+		return nil, fmt.Errorf("GetRT: s2c is nil")
+	}
+
+	result := &GetRTResponse{
+		Security: s2c.GetSecurity(),
+		Name:     s2c.GetName(),
+		RTList:   make([]*RT, 0, len(s2c.GetRtList())),
+	}
+
+	for _, rt := range s2c.GetRtList() {
+		result.RTList = append(result.RTList, &RT{
+			Time:     rt.GetTime(),
+			Price:    rt.GetPrice(),
+			Volume:   rt.GetVolume(),
+			Turnover: rt.GetTurnover(),
+			AvgPrice: rt.GetAvgPrice(),
+		})
+	}
+
+	return result, nil
+}
+
+type Broker struct {
+	ID     int64
+	Name   string
+	Pos    int32
+	Volume int64
+}
+
+type GetBrokerRequest struct {
+	Security *qotcommon.Security
+	Num      int32
+}
+
+type GetBrokerResponse struct {
+	Security      *qotcommon.Security
+	Name          string
+	AskBrokerList []*Broker
+	BidBrokerList []*Broker
+}
+
+func GetBroker(c *futuapi.Client, req *GetBrokerRequest) (*GetBrokerResponse, error) {
+	c2s := &qotgetbroker.C2S{
+		Security: req.Security,
+	}
+
+	pkt := &qotgetbroker.Request{C2S: c2s}
+
+	body, err := proto.Marshal(pkt)
+	if err != nil {
+		return nil, err
+	}
+
+	serialNo := c.NextSerialNo()
+	if err := c.Conn().WritePacket(ProtoID_GetBroker, serialNo, body); err != nil {
+		return nil, err
+	}
+
+	pktResp, err := c.Conn().ReadPacket()
+	if err != nil {
+		return nil, err
+	}
+
+	var rsp qotgetbroker.Response
+	if err := proto.Unmarshal(pktResp.Body, &rsp); err != nil {
+		return nil, err
+	}
+
+	if rsp.GetRetType() != int32(common.RetType_RetType_Succeed) {
+		return nil, fmt.Errorf("GetBroker failed: retType=%d, retMsg=%s", rsp.GetRetType(), rsp.GetRetMsg())
+	}
+
+	s2c := rsp.GetS2C()
+	if s2c == nil {
+		return nil, fmt.Errorf("GetBroker: s2c is nil")
+	}
+
+	result := &GetBrokerResponse{
+		Security:      s2c.GetSecurity(),
+		Name:          s2c.GetName(),
+		AskBrokerList: make([]*Broker, 0, len(s2c.GetBrokerAskList())),
+		BidBrokerList: make([]*Broker, 0, len(s2c.GetBrokerBidList())),
+	}
+
+	for _, b := range s2c.GetBrokerAskList() {
+		result.AskBrokerList = append(result.AskBrokerList, &Broker{
+			ID:     b.GetId(),
+			Name:   b.GetName(),
+			Pos:    b.GetPos(),
+			Volume: b.GetVolume(),
+		})
+	}
+
+	for _, b := range s2c.GetBrokerBidList() {
+		result.BidBrokerList = append(result.BidBrokerList, &Broker{
+			ID:     b.GetId(),
+			Name:   b.GetName(),
+			Pos:    b.GetPos(),
+			Volume: b.GetVolume(),
 		})
 	}
 
